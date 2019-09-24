0c3d1cb837ab10d0c8f2a0befc1097487ba9d6c8
李明哲 <963835264@qq.com>
2019-09-24 22:59:47 +0800

    翻转字符串。

---
+++ 翻转字符串/翻转字符串/main.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+#include<string>
+class solution
+{
+public:
+	string reverseString(string s)
+	{
+		if (s.empty())
+		{
+			return s;
+		}
+		int start = 0;
+		int end = s.size() - 1;
+		while (start < end)
+		{
+			swap(s[start], s[end]);
+			++start;
+			--end;
+		}
+		return s;
+	}
+};
+int main()
+{
+	string s("hello");
+	solution s1;
+	s1.reverseString(s);
+	cout << s1.reverseString(s) << endl;
+	system("pause");
+	return 0;
+}
