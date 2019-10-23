a1df7e3718a3d96d11413402d9797bac862eed0d
李明哲 <963835264@qq.com>
2019-10-23 22:51:11 +0800

    用适配器：二叉树前序遍历

--- 用适配器：二叉树的前序遍历/用适配器：二叉树的前序遍历/main.cpp
+++ 用适配器：二叉树的前序遍历/用适配器：二叉树的前序遍历/main.cpp
@@ -1,21 +1,22 @@
 #include<iostream>
 #include<queue>
 #include<vector>
+#include<stack>
 using namespace std;
-struct TreeNode
-{
-	int val;
-	TreeNode* left;
-	TreeNode* right;
-	TreeNode(int x) :val(x), left(nullptr), right(nullptr){}
-};
-//typedef struct 
+//struct TreeNode
 //{
 //	int val;
 //	TreeNode* left;
 //	TreeNode* right;
 //	TreeNode(int x) :val(x), left(nullptr), right(nullptr){}
-//}TreeNode;
+//};
+typedef struct TreeNode
+{
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode(int x) :val(x), left(nullptr), right(nullptr){}
+}TreeNode;
 class solution
 {
 	//用递归进行操作。
@@ -33,5 +34,27 @@ class solution
 			preorder_Traversal(T->left);//如果此节点有左孩子 递归调用函数
 			preorder_Traversal(T->right);//如果此节点有右孩子 递归调用函数
 		}
+	}
+};
+class solution2
+{
+	//用栈完成前序遍历
+	vector<int>preorderTraversal(TreeNode* root)
+	{
+		vector<int> v;
+		stack<TreeNode*> s;
+		TreeNode* cur = root;
+		while (cur || s.size())//当cur不为空 或者 栈里元素的个数>0
+		{
+			while (cur)
+			{
+				s.push(cur->right);//将cur节点的右孩子入栈
+				v.push_back(cur->val);//将cur节点的值放进数组中
+				cur = cur->left;//cur移向cur的左孩子，继续进行循环
+			}
+			cur=s.top();//此时栈里只剩各个节点的右孩子，cur指向其，继续进行上面循环
+			s.pop();//删除位于栈顶的右孩子
+		}
+		return v;
 	}
 };
