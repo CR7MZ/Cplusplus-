e5ea7c334bb98ebc4c60842d792ce5754e5e2009
李明哲 <963835264@qq.com>
2019-11-23 14:25:22 +0800

    完全数计算

---
+++ ]iNOC产品部--完全数计算/]iNOC产品部--完全数计算/main.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+//https://www.nowcoder.com/questionTerminal/7299c12e6abb437c87ad3e712383ff84
+
+
+int check(int n)//判断是否为完全数
+{
+	int result = 0;
+	for (int i = 1; i<n; i++)
+	{
+		if (n%i == 0)
+			result += i;
+	}
+	if (result == n)
+		return 1;
+	return 0;
+}
+int main()
+{
+	int n = 0;
+	while (cin >> n)
+	{
+		int count = 0;
+		for (int i = 1; i <= n; i++)
+		{
+			if (check(i))
+				count++;
+		}
+		cout << count << endl;
+	}
+	return 0;
+}
