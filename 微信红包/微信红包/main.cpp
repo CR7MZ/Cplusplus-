1dc2f2a84912703f83e8d89495071ce5fa7e9f20
李明哲 <963835264@qq.com>
2019-12-06 19:51:48 +0800

    微信红包

--- 微信红包/微信红包/main.cpp
+++ 微信红包/微信红包/main.cpp
@@ -1,3 +1,6 @@
+#include<iostream>
+#include<vector>
+using namespace std;
 //https://www.nowcoder.com/questionTerminal/fbcf95ed620f42a88be24eb2cd57ec54
 
 //运用排序，将数组排成有序，则数组中间的数可能为超过数组一半的数，最后遍历数组，计数中间这个数，判断其二倍是否大于数组
