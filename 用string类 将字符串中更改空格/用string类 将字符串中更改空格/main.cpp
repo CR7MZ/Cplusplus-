f8838e3b243666dfe84d15df1941a0ced6ef00c6
李明哲 <963835264@qq.com>
2019-12-14 18:16:12 +0800

    替换空格

--- 用string类 将字符串中更改空格/用string类 将字符串中更改空格/main.cpp
+++ 用string类 将字符串中更改空格/用string类 将字符串中更改空格/main.cpp
@@ -1,8 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<iostream>
 #include<string>
 using namespace std;
 //将一个字符串中的空格替换成 "%20"。
-void changestring(string& s)
+
+void replaceSpace(char *str, int length) {//方法1
+	string s(str);
+	int count = 0;
+	for (auto e : s)//统计有几个空格
+	{
+		if (e == ' ')
+			count++;
+	}
+	string result;
+	result.resize(s.size() + count * 2);//建立一个空的字符串，因为每个空格替换为%20，所以字符串大小要加count*2
+	int j = 0;
+	for (int i = 0; i<s.size(), j<result.size(); i++, j++)//两个字符串从前向后遍历
+	{
+		if (s[i] == ' ')//当遇到空格，result字符串后加%20，此时result字符串的下标+2，紧接着与i同时继续
+		{
+			result[j] = '%';
+			result[j + 1] = '2';
+			result[j + 2] = '0';
+			j += 2;
+		}
+		else{
+			result[j] = s[i];
+		}
+	}
+	auto ret = result.c_str();//因为传的参数是char*所以，要将string类型的字符串转为C类型。
+	strcpy(str, ret);
+}
+
+void changestring(string& s)//方法2
 {
 	int count = 0;
 	for (auto e : s)//先遍历字符串，统计一共有多少个空格。
@@ -31,10 +61,14 @@ void changestring(string& s)
 		p2--;
 	}
 }
+
+
 int main()
 {
-	string s("ad dasd asd");
-	changestring(s);
+	//string s("ad dasd asd");
+	//changestring(s);
+	char s[] = "hello world ca";
+	replaceSpace(s, sizeof(s));
 	cout <<s << endl;
 	system("pause");
 	return 0;
