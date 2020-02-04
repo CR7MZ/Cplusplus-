d9d5955ab942afa8255d26420f696cabc80d6077
CR7MZ <963835264@qq.com>
2020-02-04 18:02:14 +0800

    栈的压入、弹出序列

--- 栈的压入、弹出序列/栈的压入、弹出序列/main.cpp
+++ 栈的压入、弹出序列/栈的压入、弹出序列/main.cpp
@@ -2,10 +2,6 @@
 #include<stack>
 #include<vector>
 using namespace std;
-//输入两个整数序列，第一个序列表示栈的压入顺序，请判断第二个序列是否可能为该栈的弹出顺序。
-//假设压入栈的所有数字均不相等。例如序列1, 2, 3, 4, 5是某栈的压入顺序，
-//序列4, 5, 3, 2, 1是该压栈序列对应的一个弹出序列，但4, 3, 5, 1, 2就不可能是该压栈序列的弹出序列。
-//（注意：这两个序列的长度是相等的）
 class solution
 {
 public:
@@ -29,19 +25,3 @@ public:
 		return true;
 	}
 };
-void test()
-{
-	vector<int> v1{ 1, 2, 3, 4, 5 };//压入序列
-	vector<int> v2{ 4, 3, 5, 1, 2 };//弹出序列
-	solution s;
-	if (s.IsPopOrder(v1, v2))
-		cout << "是弹出序列" << endl;
-	else
-		cout << "不是弹出序列" << endl;
-}
-int main()
-{
-	test();
-	system("pause");
-	return 0;
-}
